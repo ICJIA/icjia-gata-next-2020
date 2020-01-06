e39a9ca6c5678a053d6ae29559cb89c617ff3d24
cschweda <cschweda@gmail.com>
2020-01-06 12:39:14 -0600

    fix: ie11 test

--- src/components/AppDrawer.vue
+++ src/components/AppDrawer.vue
@@ -1,15 +1,6 @@
 <template>
   <v-navigation-drawer v-model="drawer" app temporary>
-    <!-- <div class="text-center" style="background: #0D4574">
-      <img
-        src="@/assets/icjia-logo-white.png"
-        class="logo mt-4 mb-4"
-        height="40"
-        alt="Illinois Criminal Justice Information Authority"
-        @click="goto('http://www.icjia.state.il.us')"
-      />
-    </div> -->
-
+    test ie11
     <v-row>
       <v-list v-if="pages" dense>
         <div v-for="item in pages" :key="item.title">
@@ -47,56 +38,6 @@
       </v-list>
       <v-spacer></v-spacer>
     </v-row>
-
-    <!-- <div class="text-center mb-3 pt-3 py-3">
-        <img
-          src="@/assets/icjia-logo.png"
-          class="logo mt-3"
-          height="40"
-          alt="Illinois Criminal Justice Information Authority"
-          @click="goto('http://www.icjia.state.il.us')"
-        />
-        <h5 style="font-size: 12px">
-          <a
-            href="https://github.com/ICJIA/icjia-gata-next"
-            class="sidebar-footer-link"
-            >View on Github</a
-          >
-        </h5>
-        <h5 style="color: #666; font-size: 12px" class="mt-2">
-          &copy;&nbsp;2019
-          <a class="sidebar-footer-link" href="http://www.icjia.state.il.us"
-            >Illinois Criminal Justice Information Authority</a
-          >
-        </h5> -->
-    <!-- <div 
-          style="font-size: 10px;" 
-          class="mt-2"><a href="https://legacy-grants.icjia.cloud">Previous ICJIA GATA site </a>&raquo;</div>
-      </div> -->
-    <!-- </div> -->
-
-    <!-- <v-footer class="justify-center text-center py-8" app>
-      <h5 style="font-size: 12px">
-        <a href="https://github.com/ICJIA/icjia-gata-next">View on Github</a>
-      </h5>
-      <h5 style="color: #666; font-size: 12px" class="mt-2">
-        &copy;&nbsp;2019
-        <a href="http://www.icjia.state.il.us"
-          >Illinois Criminal Justice Information Authority</a
-        >
-      </h5>
-    </v-footer> -->
-    <!-- <div style="postition: absolute; bottom 0;">
-      <h5 style="font-size: 12px">
-        <a href="https://github.com/ICJIA/icjia-gata-next">View on Github</a>
-      </h5>
-      <h5 style="color: #666; font-size: 12px" class="mt-2">
-        &copy;&nbsp;2019
-        <a href="http://www.icjia.state.il.us"
-          >Illinois Criminal Justice Information Authority</a
-        >
-      </h5>
-    </div> -->
   </v-navigation-drawer>
 </template>
 
